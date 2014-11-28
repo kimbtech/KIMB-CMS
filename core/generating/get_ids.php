89717198c6fc73ccb618d146f463c217dbf3bd4d
KIMB-technologies <kimbtechnology@gmail.com>
2014-11-28 20:14:33 +0100

    KIMB CMS get IDs Teil I

--- core/generating/get_ids.php
+++ core/generating/get_ids.php
@@ -1,5 +1,33 @@
 <?php
 
-//mache aus Links oder ID => MenueID und SiteID
+//mache aus Links oder RequestID => MenueID und SiteID
+
+if( isset($_GET['url']) ){
+
+	// URL => RequestID
+
+	#dgdfgdgd
+
+}
+elseif( isset($_GET['id']) ){
+
+	// RequestID => weiter gehts ...
+
+	//nichts
+}
+else{
+
+	$_GET['id'] = '1'; // Startseite
+
+}
+
+// get MenueID
+
+#dfsdf
+
+// get SiteID
+
+
+#sdfsdfsdf
 
 ?>
