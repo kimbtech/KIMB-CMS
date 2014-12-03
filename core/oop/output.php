31f1ef38edaa60feaf8d3bfc884f0030eede2b3d
KIMB-technologies <kimbtechnology@gmail.com>
2014-12-03 20:10:16 +0100

    Issue #1 - Backend -- TeilII

--- core/oop/output.php
+++ core/oop/output.php
@@ -9,32 +9,32 @@ class system_output{
 	public function __construct($allgsysconf){
 		$this->allgsysconf = $allgsysconf;
 		$this->sonderfile = new KIMBdbf('sonder.kimb');
-		$this->footer = $this->sonderfile->read_kimb_one('footer')."\n\r";
+		$this->footer = $this->sonderfile->read_kimb_one('footer')."\r\n";
 	}
 
 	public function add_menue_one_entry($name, $link, $niveau, $clicked ){
 		if( $niveau == '1' ){
 			if( $clicked == 'yes' ){
-				$this->menue[1] .=  '<a style="color:#0000ff; background-color:#EEC900; border:solid 2px #ffffff;" class="menu" href="'.$link.'">'.$name.'</a>'."\n\r";
+				$this->menue[1] .=  '<a style="color:#0000ff; background-color:#EEC900; border:solid 2px #ffffff;" class="menu" href="'.$link.'">'.$name.'</a>'."\r\n";
 			}
 			else{
-				$this->menue[1] .=  '<a class="menu" href="'.$link.'">'.$name.'</a>'."\n\r";
+				$this->menue[1] .=  '<a class="menu" href="'.$link.'">'.$name.'</a>'."\r\n";
 			}
 		}
 		elseif( $niveau == '2'){
 			if( $clicked == 'yes' ){
-				$this->menue[2] .=  '<li><a style="color:red;" href="'.$link.'">'.$name.'</a></li>'."\n\r";
+				$this->menue[2] .=  '<li><a style="color:red;" href="'.$link.'">'.$name.'</a></li>'."\r\n";
 			}
 			else{
-				$this->menue[2] .=  '<li><a href="'.$link.'">'.$name.'</a></li>'."\n\r";
+				$this->menue[2] .=  '<li><a href="'.$link.'">'.$name.'</a></li>'."\r\n";
 			}
 		}
 		else{
 			if( $clicked == 'yes' ){
-				$this->menue[3] .=  '<li><a style="color:red;" href="'.$link.'">'.$name.'</a></li>'."\n\r";
+				$this->menue[3] .=  '<li><a style="color:red;" href="'.$link.'">'.$name.'</a></li>'."\r\n";
 			}
 			else{
-				$this->menue[3] .=  '<li><a href="'.$link.'">'.$name.'</a></li>'."\n\r";
+				$this->menue[3] .=  '<li><a href="'.$link.'">'.$name.'</a></li>'."\r\n";
 			}
 		}
 	}
@@ -44,17 +44,17 @@ class system_output{
 	}
 
 	public function add_site($content){
-		//$this->sitecontent .= $content."\n\r";
+		//$this->sitecontent .= $content."\r\n";
 		$this->set_title($content['title']);
 		$this->add_html_header($content['header']);
 		$this->add_html_header('<meta name="description" content="'.$content['description'].'">');
 		$this->add_html_header('<meta name="keywords" content="'.$content['keywords'].'">');
-		$this->sitecontent .= $content['inhalt']."\n\r";
+		$this->sitecontent .= $content['inhalt']."\r\n";
 		$this->add_footer($content['footer']);
 		$time = date( "d.m.Y" , $content['time'] );
 		$schlusszeile .= '<div style="position: absolute; bottom:2px; right:2px; border:solid 2px #aaaaaa; border-radius:2px; padding:4px;">Erstellt von '.$content['made_user'].' am '.$time.'</div>';
 		$schlusszeile .= '<div style="position: absolute; bottom:2px; left:2px; border:solid 2px #aaaaaa; border-radius:2px; padding:4px;">Permalink: <a href="'.$this->allgsysconf['siteurl'].'/index.php?id='.$content['req_id'].'">'.$this->allgsysconf['siteurl'].'/index.php?id='.$content['req_id'].'</a></div>';
-		$this->sitecontent .= $schlusszeile."\n\r";
+		$this->sitecontent .= $schlusszeile."\r\n";
 	}
 
 	public function add_site_content($content){
@@ -62,15 +62,15 @@ class system_output{
 	}
 
 	public function add_addon_area($inhalt, $style = '', $cssclass = ''){
-		$this->addon .= '<div id="menu-apps" class="'.$cssid.'" style="'.$style.'">'.$inhalt.'</div>'."\n\r";
+		$this->addon .= '<div id="menu-apps" class="'.$cssid.'" style="'.$style.'">'.$inhalt.'</div>'."\r\n";
 	}
 
 	public function add_footer($inhalt){
-		$this->footer .= $inhalt."\n\r";
+		$this->footer .= $inhalt."\r\n";
 	}
 
 	public function add_html_header($inhalt){
-		$this->header .= $inhalt."\n\r";
+		$this->header .= $inhalt."\r\n";
 	}
 
 	public function set_title($title){
@@ -80,81 +80,81 @@ class system_output{
 	public function echo_error($message = '', $art = 'unknown'){
 		if( $art == '404' ){
 			$errmsg = $this->sonderfile->read_kimb_one('error-404');
-			$this->sitecontent .= '<div id="errorbox"><h1>Error - 404</h1>'.$errmsg.'<br /><br /><i>'.$message.'</i></div>'."\n\r";
+			$this->sitecontent .= '<div id="errorbox"><h1>Error - 404</h1>'.$errmsg.'<br /><br /><i>'.$message.'</i></div>'."\r\n";
 			header("HTTP/1.0 404 Not Found");
 
 		}
 		elseif( $art == '403' ){
 			$errmsg = $this->sonderfile->read_kimb_one('error-403');
-			$this->sitecontent .= '<div id="errorbox"><h1>Error - 403</h1>'.$errmsg.'<br /><br /><i>'.$message.'</i></div>'."\n\r";
+			$this->sitecontent .= '<div id="errorbox"><h1>Error - 403</h1>'.$errmsg.'<br /><br /><i>'.$message.'</i></div>'."\r\n";
 			header('HTTP/1.0 403 Forbidden');
 		}
 		else{
-			$this->sitecontent .= '<div id="errorbox"><h1>Error - Fehler</h1>'.$message.'</div>'."\n\r";
+			$this->sitecontent .= '<div id="errorbox"><h1>Error - Fehler</h1>'.$message.'</div>'."\r\n";
 		}
 
 	}
 
 
 	public function output_complete_site(){
-		echo('<!DOCTYPE html> <html> <head>'."\n\r");
-			echo ('<title>'.$this->allgsysconf['sitename'].' : '.$this->title.'</title>'."\n\r");
-			echo ('<link rel="shortcut icon" href="'.$this->allgsysconf['sitefavi'].'" type="image/x-icon; charset=binary">'."\n\r");
-			echo ('<link rel="icon" href="'.$this->allgsysconf['sitefavi'].'" type="image/x-icon; charset=binary">'."\n\r");
-			echo ('<meta name="generator" content="KIMB-technologies CMS V. '.$this->allgsysconf['systemversion'].'" >'."\n\r");
-			echo ('<meta name="robots" content="'.$this->allgsysconf['robots'].'">'."\n\r");
-			echo ('<meta name="description" content="'.$this->allgsysconf['description'].'">'."\n\r");
-			echo ('<meta charset="utf-8">'."\n\r");
-			echo ('<link rel="stylesheet" type="text/css" href="'.$this->allgsysconf['siteurl'].'/load/system/theme/fonts.css" media="all">'."\n\r");
-			echo ('<link rel="stylesheet" type="text/css" href="'.$this->allgsysconf['siteurl'].'/load/system/theme/design.css" media="screen">'."\n\r");
-			echo ('<link rel="stylesheet" type="text/css" href="'.$this->allgsysconf['siteurl'].'/load/system/theme/print.css" media="print">'."\n\r");
+		echo('<!DOCTYPE html> <html> <head>'."\r\n");
+			echo ('<title>'.$this->allgsysconf['sitename'].' : '.$this->title.'</title>'."\r\n");
+			echo ('<link rel="shortcut icon" href="'.$this->allgsysconf['sitefavi'].'" type="image/x-icon; charset=binary">'."\r\n");
+			echo ('<link rel="icon" href="'.$this->allgsysconf['sitefavi'].'" type="image/x-icon; charset=binary">'."\r\n");
+			echo ('<meta name="generator" content="KIMB-technologies CMS V. '.$this->allgsysconf['systemversion'].'" >'."\r\n");
+			echo ('<meta name="robots" content="'.$this->allgsysconf['robots'].'">'."\r\n");
+			echo ('<meta name="description" content="'.$this->allgsysconf['description'].'">'."\r\n");
+			echo ('<meta charset="utf-8">'."\r\n");
+			echo ('<link rel="stylesheet" type="text/css" href="'.$this->allgsysconf['siteurl'].'/load/system/theme/fonts.css" media="all">'."\r\n");
+			echo ('<link rel="stylesheet" type="text/css" href="'.$this->allgsysconf['siteurl'].'/load/system/theme/design.css" media="screen">'."\r\n");
+			echo ('<link rel="stylesheet" type="text/css" href="'.$this->allgsysconf['siteurl'].'/load/system/theme/print.css" media="print">'."\r\n");
 			
 				echo($this->header);
-				echo("\n\r");
+				echo("\r\n");
 
-		echo('</head><body>'."\n\r");
-			echo('<div id="page">'."\n\r");
-				echo('<div id="header">'."\n\r");
-					echo('<a href="'.$this->allgsysconf['siteurl'].'/index.php"><div style="float:left;"><img src="'.$this->allgsysconf['siteurl'].'/load/system/theme/logo.png" style="border:none;"></div>'."\n\r");
-					echo('<div style="line-height: 100px;">'.$this->allgsysconf['sitename'].'</div></a>'."\n\r");
-				echo('</div>'."\n\r");
-				echo('<div id="menu">'."\n\r");
+		echo('</head><body>'."\r\n");
+			echo('<div id="page">'."\r\n");
+				echo('<div id="header">'."\r\n");
+					echo('<a href="'.$this->allgsysconf['siteurl'].'/index.php"><div style="float:left;"><img src="'.$this->allgsysconf['siteurl'].'/load/system/theme/logo.png" style="border:none;"></div>'."\r\n");
+					echo('<div style="line-height: 100px;">'.$this->allgsysconf['sitename'].'</div></a>'."\r\n");
+				echo('</div>'."\r\n");
+				echo('<div id="menu">'."\r\n");
 
 					echo($this->menue[1]);
-					echo("\n\r");
+					echo("\r\n");
 
-				echo('</div>'."\n\r");
-				echo('<div id="site">'."\n\r");
-					echo('<div id="menu-apps"><ul>'."\n\r");
+				echo('</div>'."\r\n");
+				echo('<div id="site">'."\r\n");
+					echo('<div id="menu-apps"><ul>'."\r\n");
 
 						echo($this->menue[2]);
-						echo("\n\r");
+						echo("\r\n");
 
 						echo('<li style="list-style-type:none" ><ul>');
 
 						echo($this->menue[3]);
-						echo("\n\r");
+						echo("\r\n");
 
 						echo('</li></ul>');
 
-					echo('</ul></div>'."\n\r");
+					echo('</ul></div>'."\r\n");
 
 						echo($this->addon);
-						echo("\n\r");
+						echo("\r\n");
 
-					echo('<div id="content-apps" style="position: relative; padding-bottom:30px;">'."\n\r");
+					echo('<div id="content-apps" style="position: relative; padding-bottom:30px;">'."\r\n");
 
 						echo($this->sitecontent);
-						echo("\n\r");
+						echo("\r\n");
 
-					echo('</div>'."\n\r");
-			echo('</div>'."\n\r");
-			echo('<div id="footer">'."\n\r");
+					echo('</div>'."\r\n");
+			echo('</div>'."\r\n");
+			echo('<div id="footer">'."\r\n");
 
 				echo($this->footer);
-				echo("\n\r");
+				echo("\r\n");
 
-			echo('</div></div>'."\n\r");
+			echo('</div></div>'."\r\n");
 		echo('</body> </html>');
 	}
 
--- core/oop/output_backend.php
+++ core/oop/output_backend.php
@@ -9,151 +9,91 @@ class backend_output{
 	public function __construct($allgsysconf){
 		$this->allgsysconf = $allgsysconf;
 		$this->sonderfile = new KIMBdbf('sonder.kimb');
-		$this->footer = $this->sonderfile->read_kimb_one('footer')."\n\r";
-	}
-
-	public function add_menue_one_entry($name, $link, $niveau, $clicked ){
-		if( $niveau == '1' ){
-			if( $clicked == 'yes' ){
-				$this->menue[1] .=  '<a style="color:#0000ff; background-color:#EEC900; border:solid 2px #ffffff;" class="menu" href="'.$link.'">'.$name.'</a>'."\n\r";
-			}
-			else{
-				$this->menue[1] .=  '<a class="menu" href="'.$link.'">'.$name.'</a>'."\n\r";
-			}
-		}
-		elseif( $niveau == '2'){
-			if( $clicked == 'yes' ){
-				$this->menue[2] .=  '<li><a style="color:red;" href="'.$link.'">'.$name.'</a></li>'."\n\r";
-			}
-			else{
-				$this->menue[2] .=  '<li><a href="'.$link.'">'.$name.'</a></li>'."\n\r";
-			}
-		}
-		else{
-			if( $clicked == 'yes' ){
-				$this->menue[3] .=  '<li><a style="color:red;" href="'.$link.'">'.$name.'</a></li>'."\n\r";
-			}
-			else{
-				$this->menue[3] .=  '<li><a href="'.$link.'">'.$name.'</a></li>'."\n\r";
-			}
-		}
-	}
-
-	public function unset_menue( $niveau ){
-		$this->menue[$niveau] = '';
-	}
-
-	public function add_site($content){
-		//$this->sitecontent .= $content."\n\r";
-		$this->set_title($content['title']);
-		$this->add_html_header($content['header']);
-		$this->add_html_header('<meta name="description" content="'.$content['description'].'">');
-		$this->add_html_header('<meta name="keywords" content="'.$content['keywords'].'">');
-		$this->sitecontent .= $content['inhalt']."\n\r";
-		$this->add_footer($content['footer']);
-		$time = date( "d.m.Y" , $content['time'] );
-		$schlusszeile .= '<div style="position: absolute; bottom:2px; right:2px; border:solid 2px #aaaaaa; border-radius:2px; padding:4px;">Erstellt von '.$content['made_user'].' am '.$time.'</div>';
-		$schlusszeile .= '<div style="position: absolute; bottom:2px; left:2px; border:solid 2px #aaaaaa; border-radius:2px; padding:4px;">Permalink: <a href="'.$this->allgsysconf['siteurl'].'/index.php?id='.$content['req_id'].'">'.$this->allgsysconf['siteurl'].'/index.php?id='.$content['req_id'].'</a></div>';
-		$this->sitecontent .= $schlusszeile."\n\r";
+		$this->footer = $this->sonderfile->read_kimb_one('footer')."\r\n";
 	}
 
 	public function add_site_content($content){
 		$this->sitecontent .= $content;
 	}
 
-	public function add_addon_area($inhalt, $style = '', $cssclass = ''){
-		$this->addon .= '<div id="menu-apps" class="'.$cssid.'" style="'.$style.'">'.$inhalt.'</div>'."\n\r";
-	}
-
-	public function add_footer($inhalt){
-		$this->footer .= $inhalt."\n\r";
-	}
-
 	public function add_html_header($inhalt){
-		$this->header .= $inhalt."\n\r";
-	}
-
-	public function set_title($title){
-		$this->title = $title;
+		$this->header .= $inhalt."\r\n";
 	}
 
 	public function echo_error($message = '', $art = 'unknown'){
 		if( $art == '404' ){
 			$errmsg = $this->sonderfile->read_kimb_one('error-404');
-			$this->sitecontent .= '<div id="errorbox"><h1>Error - 404</h1>'.$errmsg.'<br /><br /><i>'.$message.'</i></div>'."\n\r";
+			$this->sitecontent .= '<div id="errorbox"><h1>Error - 404</h1>'.$errmsg.'<br /><br /><i>'.$message.'</i></div>'."\r\n";
 			header("HTTP/1.0 404 Not Found");
 
 		}
 		elseif( $art == '403' ){
 			$errmsg = $this->sonderfile->read_kimb_one('error-403');
-			$this->sitecontent .= '<div id="errorbox"><h1>Error - 403</h1>'.$errmsg.'<br /><br /><i>'.$message.'</i></div>'."\n\r";
+			$this->sitecontent .= '<div id="errorbox"><h1>Error - 403</h1>'.$errmsg.'<br /><br /><i>'.$message.'</i></div>'."\r\n";
 			header('HTTP/1.0 403 Forbidden');
 		}
 		else{
-			$this->sitecontent .= '<div id="errorbox"><h1>Error - Fehler</h1>'.$message.'</div>'."\n\r";
+			$this->sitecontent .= '<div id="errorbox"><h1>Error - Fehler</h1>'.$message.'</div>'."\r\n";
 		}
 
 	}
 
 
 	public function output_complete_site(){
-		echo('<!DOCTYPE html> <html> <head>'."\n\r");
-			echo ('<title>'.$this->allgsysconf['sitename'].' : '.$this->title.'</title>'."\n\r");
-			echo ('<link rel="shortcut icon" href="'.$this->allgsysconf['sitefavi'].'" type="image/x-icon; charset=binary">'."\n\r");
-			echo ('<link rel="icon" href="'.$this->allgsysconf['sitefavi'].'" type="image/x-icon; charset=binary">'."\n\r");
-			echo ('<meta name="generator" content="KIMB-technologies CMS V. '.$this->allgsysconf['systemversion'].'" >'."\n\r");
-			echo ('<meta name="robots" content="none">'."\n\r");
-			echo ('<meta charset="utf-8">'."\n\r");
-			echo ('<link rel="stylesheet" type="text/css" href="'.$this->allgsysconf['siteurl'].'/load/system/theme/fonts.css" media="all">'."\n\r");
-			echo ('<link rel="stylesheet" type="text/css" href="'.$this->allgsysconf['siteurl'].'/load/system/theme/design.css" media="screen">'."\n\r");
-			echo ('<link rel="stylesheet" type="text/css" href="'.$this->allgsysconf['siteurl'].'/load/system/theme/print.css" media="print">'."\n\r");
+		echo('<!DOCTYPE html> <html> <head>'."\r\n");
+			echo ('<title>'.$this->allgsysconf['sitename'].' : Backend</title>'."\r\n");
+			echo ('<link rel="shortcut icon" href="'.$this->allgsysconf['sitefavi'].'" type="image/x-icon; charset=binary">'."\r\n");
+			echo ('<link rel="icon" href="'.$this->allgsysconf['sitefavi'].'" type="image/x-icon; charset=binary">'."\r\n");
+			echo ('<meta name="generator" content="KIMB-technologies CMS V. '.$this->allgsysconf['systemversion'].'" >'."\r\n");
+			echo ('<meta name="robots" content="none">'."\r\n");
+			echo ('<meta charset="utf-8">'."\r\n");
+			echo ('<link rel="stylesheet" type="text/css" href="'.$this->allgsysconf['siteurl'].'/load/system/theme/fonts.css" >'."\r\n");
+			echo ('<link rel="stylesheet" type="text/css" href="'.$this->allgsysconf['siteurl'].'/load/system/be.css" >'."\r\n");
+			echo ('<link rel="stylesheet" type="text/css" href="'.$this->allgsysconf['siteurl'].'/load/system/jquery/jquery-ui.min.css" >'."\r\n");
+			echo ('<script language="javascript" src="'.$this->allgsysconf['siteurl'].'/load/system/jquery/jquery.min.js"></script>'."\r\n");
+			echo ('<script language="javascript" src="'.$this->allgsysconf['siteurl'].'/load/system/jquery/jquery-ui.min.js"></script>'."\r\n");
+			echo ('<script language="javascript" src="'.$this->allgsysconf['siteurl'].'/load/system/hash.js"></script>'."\r\n");
 			
 				echo($this->header);
-				echo("\n\r");
+				echo("\r\n");
 
-		echo('</head><body>'."\n\r");
-			echo('<div id="page">'."\n\r");
-				echo('<div id="header">'."\n\r");
-					echo('<a href="'.$this->allgsysconf['siteurl'].'/index.php"><div style="float:left;"><img src="'.$this->allgsysconf['siteurl'].'/load/system/theme/logo.png" style="border:none;"></div>'."\n\r");
-					echo('<div style="line-height: 100px;">'.$this->allgsysconf['sitename'].'</div></a>'."\n\r");
-				echo('</div>'."\n\r");
-				echo('<div id="menu">'."\n\r");
+		echo('</head><body>'."\r\n");
+			echo('<div id="page">'."\r\n");
+				echo('<div id="header">'."\r\n");
+					echo("<pre>\r\n _  _____ __  __ ____         ____ __  __ ____  \r\n| |/ /_ _|  \/  | __ )       / ___|  \/  / ___| \r\n| ' / | || |\/| |  _ \ _____| |   | |\/| \___ \ \r\n| . \ | || |  | | |_) |_____| |___| |  | |___) |\r\n|_|\_\___|_|  |_|____/       \____|_|  |_|____/ \r\n</pre>"."\r\n");
+				echo('</div>'."\r\n");
+				echo('<div id="menu">'."\r\n");
 
-					echo($this->menue[1]);
-					echo("\n\r");
+					echo('<div id="tabs">
+	<ul>
+		<li><a href="#tabs-1">First</a></li>
+		<li><a href="#tabs-2">Second</a></li>
+		<li><a href="#tabs-3">Third</a></li>
+	</ul>
+	<div id="tabs-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</div>
+	<div id="tabs-2">Phasellus mattis tincidunt nibh. Cras orci urna, blandit id, pretium vel, aliquet ornare, felis. Mae.</div>
+	<div id="tabs-3">Nam dui erat, auctor a, dignissim quis, sollicitudin eu, felis. Pellentesque nisi urna, interdum ege.</div>
+</div>
 
-				echo('</div>'."\n\r");
-				echo('<div id="site">'."\n\r");
-					echo('<div id="menu-apps"><ul>'."\n\r");
+<script>$( "#tabs" ).tabs();</script>');
 
-						echo($this->menue[2]);
-						echo("\n\r");
+				echo('</div>'."\r\n");
+				echo('<div id="content">'."\r\n");
 
-						echo('<li style="list-style-type:none" ><ul>');
+					echo($this->sitecontent);
+					echo("\r\n");
 
-						echo($this->menue[3]);
-						echo("\n\r");
 
-						echo('</li></ul>');
+			echo('</div>'."\r\n");
+			echo('<div id="footer">'."\r\n");
 
-					echo('</ul></div>'."\n\r");
+				echo('Backend der Seite '.$this->allgsysconf['sitename'].' - Zugang nur für Befugte!<br /><br />');
 
-						echo($this->addon);
-						echo("\n\r");
-
-					echo('<div id="content-apps" style="position: relative; padding-bottom:30px;">'."\n\r");
-
-						echo($this->sitecontent);
-						echo("\n\r");
-
-					echo('</div>'."\n\r");
-			echo('</div>'."\n\r");
-			echo('<div id="footer">'."\n\r");
 
 				echo($this->footer);
-				echo("\n\r");
+				echo("\r\n");
 
-			echo('</div></div>'."\n\r");
+			echo('</div></div>'."\r\n");
 		echo('</body> </html>');
 	}
 
