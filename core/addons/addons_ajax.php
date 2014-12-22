dbe789263d6a6e8e334b6599225628fbf9744bab
KIMB-technologies <kimbtechnology@gmail.com>
2014-12-22 17:43:42 +0100

    Lizenzinfo

--- core/addons/addons_ajax.php
+++ core/addons/addons_ajax.php
@@ -1,5 +1,24 @@
 <?php
 
+/*************************************************/
+//KIMB-technologies
+//KIMB CMS
+//KIMB ContentManagementSystem
+//KIMB-technologies.blogspot.com
+/*************************************************/
+//CC BY-ND 4.0
+//http://creativecommons.org/licenses/by-nd/4.0/
+//http://creativecommons.org/licenses/by-nd/4.0/legalcode
+/*************************************************/
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+//BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+//IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+/*************************************************/
+
+
+
 defined('KIMB_CMS') or die('No clean Request');
 
 //includes addons die first wollen
