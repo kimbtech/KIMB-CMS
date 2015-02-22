fb0008529f386874f2924781c4eb8097eab538bd
KIMB-technologies <kimbtechnology@gmail.com>
2015-02-22 19:18:00 +0100

    Issue #16 - fertig

---
+++ core/addons/be_passw/conf_less.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('KIMB_Backend') or die('No clean Request');
+
+$sitecontent->add_site_content( '<hr /><br /><h2>Backend Passwort vergessen</h2>' );
+
+$sitecontent->add_site_content( 'Dieses Add-on fügt auf der Login-Seite des Backends einen Link "Passwort vergessen?" hinzu.' );
+$sitecontent->add_site_content( 'Dem User wird daduch die Möglichkeit gegeben unter Angabe von E-Mail-Adresse oder Username ein neues Passwort per Mail anzufordern.' );
+
+$sitecontent->add_site_content( '<br /><br /><br /><b>Weitere Einstellungen sind nicht verfügbar!</b>' );
+
+?>
---
+++ fertig/be_passw/addon/conf_more.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once( __DIR__.'/conf_less.php' );
+
+?>
