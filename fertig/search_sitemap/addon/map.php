9e3a679f0b8b19e21f2edea878bd2ebd316e97ba
KIMB-technologies <kimbtechnology@gmail.com>
2015-02-24 18:57:45 +0100

    Issue #9 bufgix & Issue #25 fertig
    Die Sitemap wird jetzt auch mit Rewrite-URL erstellt.

--- fertig/search_sitemap/addon/map.php
+++ fertig/search_sitemap/addon/map.php
@@ -31,31 +31,60 @@ if( $cacheload == 'no' ){
 
 		$niveau = $menuear['niveau'];
 
-		if ( $menuear['status'] == 'on' ){
+		if( $allgsysconf['urlrewrite'] == 'on' ){
 
 			if( !isset( $thisniveau ) ){
-				$menue .= '<li>'."\r\n";
+				$grpath = $allgsysconf['siteurl'].'/'.$menuear['path'];
 			}
 			elseif( $thisniveau == $niveau ){
-				$menue .= '</li><li>'."\r\n";
+				$grpath = substr( $grpath , '0', '-'.strlen(strrchr( $grpath , '/')));
+				$grpath = $grpath.'/'.$menuear['path'];
 			}
 			elseif( $thisniveau < $niveau ){
-				$menue .= '<ul><li>'."\r\n";
-				$thisulauf = $thisulauf + 1;
+				$grpath = $grpath.'/'.$menuear['path'];
+				$thisulaufp = $thisulauf + 1;
 			}
 			elseif( $thisniveau > $niveau ){
 				$i = 1;
 				while( $thisniveau != $niveau + $i  ){
 					$i++;
+					$grpath = substr( $grpath , '0', '-'.strlen(strrchr( $grpath , '/')));
 				}
-				$menue .= '</li>'.str_repeat( '</ul>' , $i ).'<li>'."\r\n";
-				$thisulauf = $thisulauf - $i;
+				$thisulaufp = $thisulaufp - $i;
+
+				$grpath = substr( $grpath , '0', '-'.strlen(strrchr( $grpath , '/')));
+				$grpath = substr( $grpath , '0', '-'.strlen(strrchr( $grpath , '/')));
+				$grpath = $grpath.'/'.$menuear['path'];
 			}
-
-			$menue .=  '<a href="'.$allgsysconf['siteurl'].'/index.php?id='.$menuear['requid'].'">'.$menuear['menuname'].'</a>'."\r\n";
-
-			$thisniveau = $niveau;
+			$url = $grpath.'/';
 		}
+		else{
+			$url = $allgsysconf['siteurl'].'/index.php?id='.$menuear['requid'];
+		}
+
+		if( !isset( $thisniveau ) ){
+			$menue .= '<li>'."\r\n";
+		}
+		elseif( $thisniveau == $niveau ){
+			$menue .= '</li><li>'."\r\n";
+		}
+		elseif( $thisniveau < $niveau ){
+			$menue .= '<ul><li>'."\r\n";
+			$thisulauf = $thisulauf + 1;
+		}
+		elseif( $thisniveau > $niveau ){
+			$i = 1;
+			while( $thisniveau != $niveau + $i  ){
+				$i++;
+			}
+			$menue .= '</li>'.str_repeat( '</ul>' , $i ).'<li>'."\r\n";
+			$thisulauf = $thisulauf - $i;
+		}
+
+		if ( $menuear['status'] == 'on' ){
+			$menue .=  '<a href="'.$url.'">'.$menuear['menuname'].'</a>'."\r\n";
+		}
+		$thisniveau = $niveau;
 	}
 
 	$menue .= '</li>'.str_repeat( '</ul>' , $thisulauf );
