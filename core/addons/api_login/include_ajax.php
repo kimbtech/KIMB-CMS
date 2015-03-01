c1e0917c7c3268ec4fd5a992596ee5d335099ebd
KIMB-technologies <kimbtechnology@gmail.com>
2015-03-01 12:22:23 +0100

    Issue #24 weiter

--- core/addons/api_login/include_ajax.php
+++ core/addons/api_login/include_ajax.php
@@ -5,37 +5,54 @@ defined('KIMB_CMS') or die('No clean Request');
 if( $_GET['addon'] == 'api_login' ){
 
 	$file = new KIMBdbf( 'addon/api_login__dest.kimb' );
+	$feconf = new KIMBdbf( 'addon/felogin__conf.kimb'  );
+	$loginokay = $feconf->read_kimb_one( 'loginokay' );
 
-	if( !empty( $_GET['id'] ) && is_numeric( $_GET['id'] ) ){
-		$read = $file->read_kimb_one( 'dest'.$_GET['id'] );
-		if( !empty( $read ) ){
+	if( $loginokay == $_SESSION['felogin']['loginokay'] && $_SESSION["ip"] == $_SERVER['REMOTE_ADDR'] && $_SESSION["useragent"] == $_SERVER['HTTP_USER_AGENT'] ){
 
-			$postarr['loginokay'] = $_SESSION['felogin']['loginokay'];
-			$postarr['ip'] = $_SERVER['REMOTE_ADDR'];
-			$postarr['ua'] = $_SERVER['HTTP_USER_AGENT'];
-			$postarr['gr'] = $_SESSION['felogin']['gruppe'];
-			$postarr['us'] = $_SESSION['felogin']['user'];
-			$postarr['na'] = $_SESSION['felogin']['name'];
+		if( !empty( $_GET['id'] ) && is_numeric( $_GET['id'] ) ){
+			$read = $file->read_kimb_one( 'dest'.$_GET['id'] );
+			if( !empty( $read ) ){
 			
-			echo $jsondata = json_encode( $postarr );
-			$openurl = $read.'?id='.$id;
+				$id = makepassw( 100 , '_0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz' );
 
-			$auth = $file->read_kimb_one( 'auth' );
-			$response = http_post_fields( $read , array( 'auth' => $auth, 'jsondata' => $jsondata ) );
-			if( $response == 'taken' ){
-				header( 'Location: '.$openurl );
-				die;
+				$postarr['loginokay'] = $_SESSION['felogin']['loginokay'];
+				$postarr['ip'] = $_SERVER['REMOTE_ADDR'];
+				$postarr['ua'] = $_SERVER['HTTP_USER_AGENT'];
+				$postarr['gr'] = $_SESSION['felogin']['gruppe'];
+				$postarr['us'] = $_SESSION['felogin']['user'];
+				$postarr['na'] = $_SESSION['felogin']['name'];
+				$postarr['id'] = $id;
+			
+				$jsondata = json_encode( $postarr );
+				$openurl = $read.'?id='.$id;
+
+				$auth = $file->read_kimb_one( 'auth' );
+			
+				require_once( __DIR__.'/unirest/unirest_loader.php' );
+
+				$headers = array("Accept" => "application/json");
+				$body = array( 'auth' => $auth, 'jsondata' => $jsondata );
+				$response = Unirest\Request::post( $read , $headers, $body);
+
+				if( $response->body == 'taken' ){
+					open_url( $openurl, 'outsystem' );
+					die;
+				}
+				else{
+					echo( 'Error No/Wrong Destination Answer' );
+				}
 			}
 			else{
-				echo( 'Error No/Wrong Destination Answer' );
+				echo( 'Error Destination Not Found' );
 			}
 		}
 		else{
-			echo( 'Error Destination Not Found' );
+			echo( 'Error ID Not Set' );
 		}
 	}
 	else{
-		echo( 'Error ID Not Set' );
+		echo( 'Not Logged In!' );
 	}
 	
 	die;
