2385472e5f5e06ef9e99b7bfa7e3a0a64fd09e2f
KIMB-technologies <kimbtechnology@gmail.com>
2015-03-31 18:06:45 +0200

    Issue #33 Anfang

--- core/conf/funktionen.php
+++ core/conf/funktionen.php
@@ -538,4 +538,147 @@ function add_tiny( $big = false, $small = false, $ids = array( 'big' => '#inhalt
 
 	$sitecontent->add_html_header('</script>');
 }
+
+function compare_cms_vers( $v1 , $v2 ) {
+
+	$v[0] = $v1;
+	$v[1] = $v2;
+
+	foreach( $v as $ver ){
+
+		//Ganze erste Nummer
+		$vpos = stripos( $ver , 'V' );
+		$ppos = strpos( $ver , '.', $vpos );
+
+		$lv = $ppos - $vpos;
+
+		$teil['eins'] = substr( $ver , $vpos + 1 , $lv - 1 );
+
+		//Kommastelle & A,B,F
+		$ppos = strpos( $ver , '.' , $vpos );
+		$apos = stripos( $ver , 'A', $ppos );
+		$bpos = stripos( $ver , 'B', $ppos );
+		$fpos = stripos( $ver , 'F', $ppos );
+
+		if ( $apos !== false ){
+			$lpos = $apos;
+			$teil['bst'] = '1';
+		}
+		elseif( $bpos !== false ){
+			$lpos = $bpos;
+			$teil['bst'] = '2';
+		}
+		elseif( $fpos !== false ){
+			$lpos = $fpos;
+			$teil['bst'] = '3';
+		}
+		else{
+			return false;
+		}
+
+		$lv = $lpos - $ppos;
+
+		$teil['komma'] = substr( $ver , $ppos + 1 , $lv - 1 );
+
+		//Patch
+		$papos = stripos( $ver , '-p', $lpos );
+	
+		$patch = substr( $ver , $papos + 2 );
+
+		$kpos = strrpos( $patch , ',' );
+
+		if( $kpos !== false ){
+			$patch = substr( $patch , $kpos + 1 );
+		}
+
+		$patch = preg_replace( "/\D/", '', $patch );  
+
+		$teil['patch'] = $patch;
+
+		//fertig
+
+		foreach( $teil as $tei ){
+			if( !is_numeric( $tei ) ){
+				return false;
+			}
+		}
+
+		$varr[] = $teil;
+	}
+
+	//Ganze erste Nummer
+	if( $varr[0]['eins'] > $varr[1]['eins'] ){
+
+		return 'newer';
+
+	}
+	elseif( $varr[0]['eins'] < $varr[1]['eins'] ){
+
+		return 'older';
+
+	}
+	elseif( $varr[0]['eins'] == $varr[1]['eins'] ){
+
+		//Kommastelle
+		if( $varr[0]['komma'] > $varr[1]['komma'] ){
+
+			return 'newer';
+
+		}
+		elseif( $varr[0]['komma'] < $varr[1]['komma'] ){
+
+			return 'older';
+
+		}
+		elseif( $varr[0]['komma'] == $varr[1]['komma'] ){
+
+			//A,B,F
+			if( $varr[0]['bst'] > $varr[1]['bst'] ){
+
+				return 'newer';
+
+			}
+			elseif( $varr[0]['bst'] < $varr[1]['bst'] ){
+
+				return 'older';
+
+			}
+			elseif( $varr[0]['bst'] == $varr[1]['bst'] ){
+
+				//Patch
+				if( $varr[0]['patch'] > $varr[1]['patch'] ){
+
+					return 'newer';
+
+				}
+				elseif( $varr[0]['patch'] < $varr[1]['patch'] ){
+
+					return 'older';
+
+				}
+				elseif( $varr[0]['patch'] == $varr[1]['patch'] ){
+
+					return 'same';
+
+				}
+				else{
+					return false;
+				}
+
+			}
+			else{
+				return false;
+			}
+
+		}
+		else{
+			return false;
+		}
+
+	}
+	else{
+		return false;
+	}
+}
+
 ?>
--- kimb-cms-backend/addon_inst.php
+++ kimb-cms-backend/addon_inst.php
@@ -55,13 +55,13 @@ if( isset( $_GET['del'] ) && isset( $_GET['addon'] ) ){
 elseif( isset( $_FILES['userfile']['name'] ) ){
 
 	if( !mkdir( __DIR__.'/../core/addons/temp/' ) ){
-		$sitecontent->echo_error( 'Die Installation schlug fehl!' , 'unknown');
+		$sitecontent->echo_error( 'Es konnte kein Installationsverzeichnis erstellt werden!' , 'unknown');
 		$sitecontent->output_complete_site();
 		die;
 	}
 
 	if( !chmod( __DIR__.'/../core/addons/temp/' , ( fileperms( __DIR__.'/../core/addons/' ) & 0777) ) ){
-		$sitecontent->echo_error( 'Die Installation schlug fehl!' , 'unknown');
+		$sitecontent->echo_error( 'Es konnte kein Installationsverzeichnis eingerichtet werden!' , 'unknown');
 		$sitecontent->output_complete_site();
 		die;
 	}
@@ -72,21 +72,51 @@ elseif( isset( $_FILES['userfile']['name'] ) ){
 	    $zip->close();
 	}
 	else{
-		$sitecontent->echo_error( 'Die Installation schlug fehl!' , 'unknown');
+		$sitecontent->echo_error( 'Die Add-on Datei konnte nicht geöffnet werden!' , 'unknown');
 		$sitecontent->output_complete_site();
 		die;
 	}
 
-	$name = file_get_contents( __DIR__.'/../core/addons/temp/name.info' );
-	$name = preg_replace("/[^a-z_]/","", $name);
+	$addonini = parse_ini_file( __DIR__.'/../core/addons/temp/add-on.ini' , true);
+
+	$name = $addonini['inst']['name'];
+
+	if( $name == 'temp' ){
+		rm_r( __DIR__.'/../core/addons/temp/' );
+		$sitecontent->echo_error( 'Ein Add-on darf nicht "temp" heißen!' );
+		$sitecontent->output_complete_site();
+		die;
+	}
+
+	if( compare_cms_vers( $addonini['inst']['mincmsv'], $allgsysconf['build'] ) == 'newer' ){
+		rm_r( __DIR__.'/../core/addons/temp/' );
+		$sitecontent->echo_error( 'Sie haben eine zu alte Version des CMS für das Add-on "'.$name.'" !' );
+		$sitecontent->output_complete_site();
+		die;
+	}
+	if( compare_cms_vers( $allgsysconf['build'], $addonini['inst']['maxcmsv'] ) == 'newer' ){
+		rm_r( __DIR__.'/../core/addons/temp/' );
+		$sitecontent->echo_error( 'Sie haben eine zu neue Version des CMS für das Add-on "'.$name.'" !' );
+		$sitecontent->output_complete_site();
+		die;
+	}
 
 	if( is_dir( __DIR__.'/../core/addons/'.$name.'/' ) ){
-		rm_r( __DIR__.'/../core/addons/temp/' );
-		$sitecontent->echo_error( 'Das Add-on "'.$name.'" ist bereits installiert!' , 'unknown');
-		$sitecontent->output_complete_site();
-		die;
+		
+		$oldini = parse_ini_file( __DIR__.'/../core/addons/'.$name.'/add-on.ini' , true);
+
+		if( compare_cms_vers( $oldini['inst']['addonversion'], $addonini['inst']['addonversion'] ) == 'older' ){
+			$sitecontent->echo_message( 'Das Add-on "'.$name.'" wurde mit dieser Installation aktualisiert!' );
+		}
+		else{
+			rm_r( __DIR__.'/../core/addons/temp/' );
+			$sitecontent->echo_error( 'Das Add-on "'.$name.'" ist bereits installiert!' );
+			$sitecontent->output_complete_site();
+			die;
+		}
 	}
 
+	copy( __DIR__.'/../core/addons/temp/add-on.ini', __DIR__.'/../core/addons/temp/addon/add-on.ini' );
 	copy_r( __DIR__.'/../core/addons/temp/addon' , __DIR__.'/../core/addons/'.$name.'/' );
 	copy_r( __DIR__.'/../core/addons/temp/load' , __DIR__.'/../load/addondata/'.$name.'/' );
 
@@ -95,7 +125,7 @@ elseif( isset( $_FILES['userfile']['name'] ) ){
 	}
 
 	if( !rm_r( __DIR__.'/../core/addons/temp/' ) ){
-		$sitecontent->echo_error( 'Die Installation schlug fehl!' , 'unknown');
+		$sitecontent->echo_error( 'Die Installationsdateien konnte nicht gelöscht werden!' );
 		$sitecontent->output_complete_site();
 		die;
 	}
@@ -143,63 +173,151 @@ elseif( $_GET['todo'] == 'chdeak' && isset( $_GET['addon'] ) ){
 
 	$sitecontent->echo_message( 'Der Status des Add-ons "'.$_GET['addon'].'" wurde geändert!' );
 }
+elseif( $_GET['todo'] == 'addonnews' && isset( $_GET['addon'] ) ){
 
-$sitecontent->add_html_header('<script>
-var del = function( addon ) {
-	$( "#del-confirm" ).show( "fast" );
-	$( "#del-confirm" ).dialog({
-	resizable: false,
-	height:200,
-	modal: true,
-	buttons: {
-		"Delete": function() {
-			$( this ).dialog( "close" );
-			window.location = "'.$allgsysconf['siteurl'].'/kimb-cms-backend/addon_inst.php?addon=" + addon + "&del";
-			return true;
-		},
-		Cancel: function() {
-			$( this ).dialog( "close" );
-			return false;
-		}
-	}
-	});
+	$oldini = parse_ini_file( __DIR__.'/../core/addons/'.$_GET['addon'].'/add-on.ini' , true);
+
+	$sitecontent->add_site_content('<h2>Infos zum Add-on "'.$oldini['about']['name'].'"</h2>');
+
+	$sitecontent->add_site_content('<ul>');
+	$sitecontent->add_site_content('<li><b>Name:</b> '.$oldini['about']['name'].'</li>');
+	$sitecontent->add_site_content('<li><b>Version:</b> '.$oldini['inst']['addonversion'].'</li>');
+	$sitecontent->add_site_content('<li><b>CMS Version (min - max):</b> '.$oldini['inst']['mincmsv'].' - '.$oldini['inst']['maxcmsv'].'</li>');
+	$sitecontent->add_site_content('<li></li>');
+	$sitecontent->add_site_content('<li><b>By:</b> '.$oldini['about']['by'].'</li>');
+	$sitecontent->add_site_content('<li><b>Lizenz:</b> '.$oldini['about']['lic'].'</li>');
+	$sitecontent->add_site_content('<li><b>Homepage (für z.B. Updates):</b> <a href="'.$oldini['about']['url'].'" target="_blank">Besuchen!</a></li>');
+	$sitecontent->add_site_content('</ul>');
+
+	$sitecontent->add_site_content('<br /><br /><a href="'.$allgsysconf['siteurl'].'/kimb-cms-backend/addon_inst.php">Zur Add-on Seite</a>');
+
+
+	$showlist = 'no';
 }
-</script>');
+elseif( $_GET['todo'] == 'checkall' ){
 
+	if( ini_get( 'allow_url_fopen' ) ){
 
-$sitecontent->add_site_content('<h2>Add-onliste</h2>');
-$sitecontent->add_site_content('<table width="100%"><tr> <th>Name</th> <th>Status</th> <th>Löschen</th> </tr>');
-$addons = listaddons();
-foreach( $addons as $addon ){
+		$addons = listaddons();
 
-	$del = '<span onclick="var delet = del( \''.$addon.'\' ); delet();"><span class="ui-icon ui-icon-trash" title="Dieses Add-on löschen." style="display:inline-block;" ></span></span>';
+		if( empty( $addoninclude->read_kimb_one( 'lastcheck' ) ) ){
+			$addoninclude->write_kimb_new( 'lastcheck', time() );
+		}
+		else{
+			$addoninclude->write_kimb_replace( 'lastcheck', time() );
+		}
 
-	if ( $addoninclude->read_kimb_search_teilpl( 'ajax' , $addon ) || $addoninclude->read_kimb_search_teilpl( 'backend' , $addon ) || $addoninclude->read_kimb_search_teilpl( 'first' , $addon ) || $addoninclude->read_kimb_search_teilpl( 'second' , $addon ) ){
-		$status = '<a href="'.$allgsysconf['siteurl'].'/kimb-cms-backend/addon_inst.php?todo=chdeak&amp;addon='.$addon.'"><span class="ui-icon ui-icon-check" style="display:inline-block;" title="Dieses Add-on ist zu Zeit aktiviert. ( click -> ändern )"></span></a>';
+		foreach( $addons as $addon ){
+			$ver = json_decode( file_get_contents( 'http://api.kimb-technologies.eu/cms/addon/getcurrentversion.php?addon='.$addon ) , true );
+			$oldini = parse_ini_file( __DIR__.'/../core/addons/'.$addon.'/add-on.ini' , true);
+
+			if( compare_cms_vers( $ver['aktuell'], $oldini['inst']['addonversion'] ) == 'newer' && $ver['err'] == 'no' ){
+				$addoninclude->write_kimb_id( '21' , 'add' , $addon , 'upd' );
+			}
+			elseif( $ver['err'] == 'no' ){
+				$addoninclude->write_kimb_id( '21' , 'add' , $addon , 'noup' );
+			}
+			else{
+				$addoninclude->write_kimb_id( '21' , 'add' , $addon , '---empty---' );
+			}
+		}
+
+		$sitecontent->echo_message( 'Die Aktualität der Add-ons wurde überprüft!' );
 	}
 	else{
-		$status = '<a href="'.$allgsysconf['siteurl'].'/kimb-cms-backend/addon_inst.php?todo=chdeak&amp;addon='.$addon.'"><span class="ui-icon ui-icon-close" style="display:inline-block;" title="Dieses Add-on ist zu Zeit deaktiviert. ( click -> ändern )"></span></a>';
+		$sitecontent->echo_message( 'Ihr Server erlaubt PHP keine Requests per HTTP zu anderen Servern!' );
 	}
+}
+
+if( is_dir( __DIR__.'/../core/addons/temp/' ) ){
+	rm_r( __DIR__.'/../core/addons/temp/' );
+}
+
+if( !isset( $showlist ) ){
+	$sitecontent->add_html_header('<script>
+	var del = function( addon ) {
+		$( "#del-confirm" ).show( "fast" );
+		$( "#del-confirm" ).dialog({
+		resizable: false,
+		height:200,
+		modal: true,
+		buttons: {
+			"Delete": function() {
+				$( this ).dialog( "close" );
+				window.location = "'.$allgsysconf['siteurl'].'/kimb-cms-backend/addon_inst.php?addon=" + addon + "&del";
+				return true;
+			},
+			Cancel: function() {
+				$( this ).dialog( "close" );
+				return false;
+			}
+		}
+		});
+	}
+	</script>');
+
+
+	$sitecontent->add_site_content('<h2>Add-onliste</h2>');
+	$sitecontent->add_site_content('<table width="100%"><tr> <th>Name</th> <th>Status</th> <th>Updates</th> <th>Löschen</th> </tr>');
+	$addons = listaddons();
+
+	$lastcheck = $addoninclude->read_kimb_one( 'lastcheck' );
+	if( $lastcheck + 259200 > time() ){
+		$updinfos = $addoninclude->read_kimb_id( '21' );
+	}
+	else{
+		$updinfos = array();
+	}
+
+	foreach( $addons as $addon ){
+
+		$oldini = parse_ini_file( __DIR__.'/../core/addons/'.$addon.'/add-on.ini' , true);
+
+		$link = '<a href="'.$allgsysconf['siteurl'].'/kimb-cms-backend/addon_inst.php?todo=addonnews&addon='.$addon.'">'.$oldini['about']['name'].'</a>';
+
+		$del = '<span onclick="var delet = del( \''.$addon.'\' ); delet();"><span class="ui-icon ui-icon-trash" title="Dieses Add-on löschen." style="display:inline-block;" ></span></span>';
+
+		if ( $addoninclude->read_kimb_search_teilpl( 'ajax' , $addon ) || $addoninclude->read_kimb_search_teilpl( 'backend' , $addon ) || $addoninclude->read_kimb_search_teilpl( 'first' , $addon ) || $addoninclude->read_kimb_search_teilpl( 'second' , $addon ) ){
+			$status = '<a href="'.$allgsysconf['siteurl'].'/kimb-cms-backend/addon_inst.php?todo=chdeak&amp;addon='.$addon.'"><span class="ui-icon ui-icon-check" style="display:inline-block;" title="Dieses Add-on ist zu Zeit aktiviert. ( click -> ändern )"></span></a>';
+		}
+		else{
+			$status = '<a href="'.$allgsysconf['siteurl'].'/kimb-cms-backend/addon_inst.php?todo=chdeak&amp;addon='.$addon.'"><span class="ui-icon ui-icon-close" style="display:inline-block;" title="Dieses Add-on ist zu Zeit deaktiviert. ( click -> ändern )"></span></a>';
+		}
+
+		if( empty( $updinfos[$addon] ) ){
+			$upd = '<a href="'.$allgsysconf['siteurl'].'/kimb-cms-backend/addon_inst.php?todo=checkall"><span class="ui-icon ui-icon-help" style="display:inline-block;" title="Bitte klicken Sie für eine Abfrage! ( Wenn Sie gerade eine Abfrage gemacht haben, ist dieses Add-on wohl nicht in der Datenbank! )"></span></a>';
+		}
+		elseif( $updinfos[$addon] == 'noup' ){
+			$upd = '<a href="'.$allgsysconf['siteurl'].'/kimb-cms-backend/addon_inst.php?todo=checkall"><span class="ui-icon ui-icon-check" style="display:inline-block;" title="Das Add-on scheint aktuell zu sein!"></span></a>';
+		}
+		elseif( $updinfos[$addon] == 'upd' ){
+			$upd = '<a href="'.$allgsysconf['siteurl'].'/kimb-cms-backend/addon_inst.php?todo=checkall"><span class="ui-icon ui-icon-alert" style="display:inline-block;" title="Es gibt ein Update für dieses Add-on!"></span>';
+		}
 			
-	$sitecontent->add_site_content('<tr> <td>'.$addon.'</td> <td>'.$status.'</td> <td>'.$del.'</td> </tr>');
+		$sitecontent->add_site_content('<tr> <td>'.$link.'</td> <td>'.$status.'</td> <td>'.$upd.'</td> <td>'.$del.'</td> </tr>');
 
-	$liste = 'yes';
+		$liste = 'yes';
 
+	}
+	$sitecontent->add_site_content('</table>');
+
+	if( $updchecked == 'yes' ){
+		$addoninclude->write_kimb_replace( 'lastcheck' , time() );
+	}
+
+	if( $liste != 'yes' ){
+		$sitecontent->echo_error( 'Es wurden keine Add-ons gefunden!' );
+	}
+
+	$sitecontent->add_site_content('<div style="display:none;"><div id="del-confirm" title="Löschen?"><p><span class="ui-icon ui-icon-alert" style="float:left; margin:0 7px 20px 0;"></span>Möchten Sie dieses Addon wirklich löschen?</p></div></div>');
+
+
+	$sitecontent->add_site_content('<br /><br /><h2>Add-on installieren</h2>');
+	$sitecontent->add_site_content('<form action="'.$allgsysconf['siteurl'].'/kimb-cms-backend/addon_inst.php" enctype="multipart/form-data" method="post">');
+	$sitecontent->add_site_content('<input name="userfile" type="file" /><br />');
+	$sitecontent->add_site_content('<input type="submit" value="Installieren" title="Wählen Sie eine Add-on Zip Datei von Ihrem Rechner zur Installation." />');
+	$sitecontent->add_site_content('</form>');
 }
-$sitecontent->add_site_content('</table>');
-
-if( $liste != 'yes' ){
-	$sitecontent->echo_error( 'Es wurden keine Add-ons gefunden!' );
-}
-
-$sitecontent->add_site_content('<div style="display:none;"><div id="del-confirm" title="Löschen?"><p><span class="ui-icon ui-icon-alert" style="float:left; margin:0 7px 20px 0;"></span>Möchten Sie dieses Addon wirklich löschen?</p></div></div>');
-
-
-$sitecontent->add_site_content('<br /><br /><h2>Add-on installieren</h2>');
-$sitecontent->add_site_content('<form action="'.$allgsysconf['siteurl'].'/kimb-cms-backend/addon_inst.php" enctype="multipart/form-data" method="post">');
-$sitecontent->add_site_content('<input name="userfile" type="file" /><br />');
-$sitecontent->add_site_content('<input type="submit" value="Installieren" title="Wählen Sie eine Add-on Zip Datei von Ihrem Rechner zur Installation." />');
-$sitecontent->add_site_content('</form>');
 
 $sitecontent->output_complete_site();
 ?>
