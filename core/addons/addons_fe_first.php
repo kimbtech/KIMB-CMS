0be6ad68a98a153c4f4dcb47481ce13434888e14
KIMB-technologies <kimbtechnology@gmail.com>
2015-04-29 17:42:53 +0200

    Issue #34 - Fortsetzung

--- core/addons/addons_fe_first.php
+++ core/addons/addons_fe_first.php
@@ -17,8 +17,6 @@
 //IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 /*************************************************/
 
-
-
 defined('KIMB_CMS') or die('No clean Request');
 
 //includes addons die first wollen
@@ -30,6 +28,63 @@ if( !isset( $addoninclude ) ){
 }
 
 $all = $addoninclude->read_kimb_all_teilpl( 'fe_first' );
+
+$addonwish = new KIMBdbf('addon/wish/fe_all.kimb');
+
+$includes = array();
+
+foreach( $all as $add ){
+
+	$id = $addonwish->search_kimb_xxxid( $add , 'addon' );
+
+	if( $id != false ){
+
+		//IDs
+		$wid = $addonwish->read_kimb_id( $id, 'ids' );
+		$wids = false;
+		$ebst = substr( $wid, 0, 1 );
+		$wid = substr( $wid, 1);
+
+		if( $ebst == 'a' ){
+			$wids = true;
+		}
+		elseif( $ebst == 'r' && $_GET['id'] == $wid ){
+			$wids = true;
+		}
+		elseif( $ebst == 's' && $allgsiteid == $wid ){
+			$wids = true;
+		}
+
+		//Error
+		$err = $addonwish->read_kimb_id( $id, 'error' );
+		$error = false;
+
+		if( $err == 'all' ){
+			$error = true;
+		}
+		elseif( $err == 'no' && $allgerr != '404' && $allgerr != '403' ){
+			$error = true;
+		}
+		elseif( $err == '404' && $allgerr == '404' ){
+			$error = true;
+		}
+		elseif( $err == '403' && $allgerr == '403' ){
+			$error = true;
+		}
+
+		if( $error && $wids ){
+			//Reihenfolge
+			$wi = $addonwish->read_kimb_id( $id, 'stelle' );
+			if( $wi == 'vorn' ){
+				array_unshift( $includes , $add );
+			}
+			elseif( $wi == 'hinten' ){
+				$includes[] = $add;
+			}
+		}
+	}
+
+}
 
 //Machen!!!!!
 //Machen!!!!!
