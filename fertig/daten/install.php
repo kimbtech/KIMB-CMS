b3812f1f8b5dd163e6601055601b833c599ac502
KIMB-technologies <kimbtechnology@gmail.com>
2015-11-26 18:21:18 +0100

    Issue #61 fertig

---
+++ fertig/daten/install.php
@@ -0,0 +1,15 @@
+<?php
+
+//Abhängikeiten
+if( !is_dir( __DIR__.'/../felogin/' ) ){
+
+	$sitecontent->echo_error( 'Das Add-on "felogin" wurde nicht gefunden, bitte installieren und aktivieren Sie es um eine ordnungsgemäße Funktion von "daten" zu gewährleisten! ' , 'unknown' );
+
+}
+elseif( !$this->addoninclude->read_kimb_search_teilpl( 'ajax' , 'felogin' ) ){
+
+	$sitecontent->echo_error( 'Das Add-on "felogin" scheint nicht aktiviert zu sein, bitte aktivieren Sie es um eine ordnungsgemäße Funktion von "daten" zu gewährleisten! ' , 'unknown' );
+
+}
+
+?>
