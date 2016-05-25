9ed11da496bc5d07c1ded93ab7e5aa3d4614decd
KIMB-technologies <kimbtechnology@gmail.com>
2016-05-25 19:13:44 +0200

    Pack V2.5 Beginn

--- core/addons/api_login/unirest/unirest_loader.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once ( __DIR__.'/File.php' );
-require_once ( __DIR__.'/Method.php' );
-require_once ( __DIR__.'/Response.php' );
-require_once ( __DIR__.'/Request.php' );
-
-?>
--- start.sh
+++
@@ -1,3 +0,0 @@
-firefox http://cms.dev:8000 &
-gnome-terminal &
-php -S cms.dev:8000
